fecc0450e18e223cd9eed0ab9a41154394e9b3e0
makariosm <mansour.ma@northeastern.edu>
2026-04-02 17:34:54 -0400

    console debugging

--- kambaz/users/dao.js
+++ kambaz/users/dao.js
@@ -1,7 +1,6 @@
 import model from "./model.js";
 import { v4 as uuidv4 } from "uuid";
 export default function UsersDao() {
-  
   const createUser = (user) => {
     const newUser = { ...user, _id: uuidv4() };
     return model.create(newUser);
@@ -11,8 +10,12 @@ export default function UsersDao() {
   const findUserById = (userId) => model.findById(userId);
   const findUserByUsername = (username) =>
     model.findOne({ username: username });
-  const findUserByCredentials = (username, password) =>
-    model.findOne({ username, password });
+  const findUserByCredentials = (username, password) => {
+    console.log("username:", username);
+    console.log("password:", password);
+    console.log("ALL USERS:", model.find());
+    return model.findOne({ username, password });
+  };
   const updateUser = (userId, user) =>
     model.updateOne({ _id: userId }, { $set: user });
   const deleteUser = (userId) => model.findByIdAndDelete(userId);
